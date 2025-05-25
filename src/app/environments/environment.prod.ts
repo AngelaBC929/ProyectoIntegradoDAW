ab43873d70adf622f598b2cf25bd169d289c8cb4
Angela <98353522+AngelaBC929@users.noreply.github.com>
2025-05-25 18:58:59 +0200

    Corrijo rutas para producción

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://tu-backend-en-produccion.com/api' // <-- Cambia esta URL cuando despliegues el backend
-  };
-  
+  production: true,
+  API_URL: "https://expresscapturevz.wuaze.com/backendRallyFotografico"
+};
